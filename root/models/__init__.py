11fd88120d652a5f54884681a28617fc0954837c
viacheslav.sabadash <viacheslav.sabadash@wecandevelopit.com>
2022-01-25 17:29:59 +0300

    added AdSpot model

--- root/models/__init__.py
+++ root/models/__init__.py
@@ -1,3 +1,4 @@
 from .base import Base
 from .publisher import Publisher
 from .adspot_type import AdSpotType
+from .adspot import AdSpot
---
+++ root/models/adspot.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+from root import models
+
+
+class AdSpot(models.Base):
+    __tablename__ = 'adspots'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String, nullable=False)
+    publisher_id = Column(Integer)
+    adspot_type_id = Column(Integer)
+    ad_metadata = Column(String)
+
+    def __init__(
+            self,
+            name,
+            publisher_id,
+            adspot_type_id,
+            metadata,
+    ):
+        self.name = name
+        self.publisher_id = publisher_id
+        self.adspot_type_id = adspot_type_id
+        self.metadata = metadata
