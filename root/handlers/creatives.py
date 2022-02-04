e61ae26ccbf14d6f19a7def3b6279a11d37a13a6
viacheslav.sabadash <viacheslav.sabadash@wecandevelopit.com>
2022-02-04 14:48:15 +0300

    fix misc

--- root/handlers/creatives.py
+++ root/handlers/creatives.py
@@ -15,10 +15,3 @@ class CreativesHandler(BaseHandler):
         else:
             self.ms.add_creative(creative)
             await self.send_ok()
-
-    async def delete(self):
-        id_s = self.json_args.get('id_s', [])
-        if not isinstance(id_s, list):
-            id_s = [id_s]
-        self.ms.delete_creatives(id_s)
-        await self.send_ok()
